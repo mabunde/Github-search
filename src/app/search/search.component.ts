f826e9d9cc6fefbbbc3e1de4aa7a4dc245f65d7a
mabunde <solomonsitati39@gmail.com>
2021-10-18 21:48:30 +0300

    Add search method

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfileService } from '../profile.service';
+
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+    username: string;
+    isNotEmpty: boolean;
+    profileService: ProfileService;
 
-  ngOnInit(): void {
+  constructor(dataService: ProfileService, private router: Router) {
+    this.profileService = dataService;
+   }
+
+  startSearch(){
+    if(this.username){
+        this.profileService.getData(this.username);
+        this.router.navigate(['../results']);
+    }else{
+        this.isNotEmpty = true;
+    }
+  }
+  hideAlert(){
+      this.isNotEmpty = false;
   }
 
+  ngOnInit(): void {
+      this.isNotEmpty = false;
+
+}
 }
