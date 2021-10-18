0f046c0412d509de6e462e597fcce8770a03f38e
mabunde <solomonsitati39@gmail.com>
2021-10-18 21:20:44 +0300

    Create router constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+export let browserRefresh = false;
 export class AppComponent {
   title = 'Github-search';
+  subscription: Subscription;
+
+  constructor(private router: Router){
+    this.subscription = router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        browserRefresh = !router.navigated;
+      }
+  });
+}
 }
