8b382d48adae708c220151921a3bdd34ff76af62
mabunde <solomonsitati39@gmail.com>
2021-10-18 05:38:32 +0300

    Add user and repository constructors

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,9 @@
 export class Repository {
+    constructor(
+        public name:string,
+        public description:string,
+        public forks_count:number,
+        public language:string,
+        public html_url:string
+    ){}
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,15 @@
 export class User {
+    constructor(public avatar_url: any,
+        public name:string,
+        public login: string,
+        public followers_url: number,
+        public following_url: number,
+        public repos_url: string,
+        public public_repos: string,
+        public bio:string,
+        public created_at:Date,
+        public updated_at:Date
+       ){}
+  
+
 }
