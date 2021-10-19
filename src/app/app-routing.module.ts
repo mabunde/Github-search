a91685a284f3b51a0be18d6ff946e03dfa4d76e3
mabunde <solomonsitati39@gmail.com>
2021-10-19 13:52:41 +0300

    Add routing in the nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'search', component: SearchComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: '404', component: NotFoundComponent},
+  { path: '', redirectTo:'profile', pathMatch:'full'},
+  { path: '**', redirectTo:'404', pathMatch:'full'}
+]
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [ RouterModule]
 })
 export class AppRoutingModule { }
