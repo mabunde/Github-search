40b4d22c73da3ef331ddf0885ff6a9f6c827d470
mabunde <solomonsitati39@gmail.com>
2021-10-19 08:10:48 +0300

    import bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NavigationStart, Router } from '@angular/router';
 
-
+export let browserRefresh = false;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export let browserRefresh = false;
+
 export class AppComponent {
   title = 'Github-search';
   subscription: Subscription;
